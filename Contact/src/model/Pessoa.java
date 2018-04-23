0b997a83bba5607c9def64f82da77e652cd81f9d
Andrey <Andrey@SHATE>
2018-04-22 21:30:58 -0300

    Detectar Mundanças na Seleção da Tabela.

--- Contact/src/model/Pessoa.java
+++ Contact/src/model/Pessoa.java
@@ -9,59 +9,55 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-public class Pessoa {	
+public class Pessoa {
 
-	    private final StringProperty primeiroNome;
-	    private final StringProperty segundoNome;
-	    private final StringProperty rua;
-	    private final IntegerProperty cep;
-	    private final StringProperty cidade;
-	    private final ObjectProperty<LocalDate> aniversario;
+	private final StringProperty primeiroNome;
+	private final StringProperty segundoNome;
+	private final StringProperty rua;
+	private final IntegerProperty cep;
+	private final StringProperty cidade;
+	private final ObjectProperty<LocalDate> aniversario;
 
-	    
-	    public Pessoa(String primeiroNome, String segundoNome) {
-			this.primeiroNome = new SimpleStringProperty(primeiroNome);
-			this.segundoNome = new SimpleStringProperty(segundoNome);
-			this.rua = new SimpleStringProperty("Rua teste");
-			this.cep = new SimpleIntegerProperty(1234);			
-			this.cidade = new SimpleStringProperty("Goiania");
-			this.aniversario = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
-		}
-	    	    
-	 
-		/**
-	     *  Construtor padrão.
-	     */
-	    public Pessoa() {
-	        this(null, null);
-	    }
-	
-	    
-	    //GETERS E SETERS
-	    
-	    
-	    public StringProperty getPrimeiroNome() {
-			return primeiroNome;
-		}
+	public Pessoa(String primeiroNome, String segundoNome) {
+		this.primeiroNome = new SimpleStringProperty(primeiroNome);
+		this.segundoNome = new SimpleStringProperty(segundoNome);
+		this.rua = new SimpleStringProperty("Rua teste");
+		this.cep = new SimpleIntegerProperty(1234);
+		this.cidade = new SimpleStringProperty("Goiania");
+		this.aniversario = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 21));
+	}
 
-		public StringProperty getSegundoNome() {
-			return segundoNome;
-		}
+	/**
+	 * Construtor padrão.
+	 */
+	public Pessoa() {
+		this(null, null);
+	}
 
-		public StringProperty getRua() {
-			return rua;
-		}
+	// GETERS E SETERS
 
-		public IntegerProperty getCep() {
-			return cep;
-		}
+	public StringProperty getPrimeiroNome() {
+		return primeiroNome;
+	}
 
-		public StringProperty getCidade() {
-			return cidade;
-		}
+	public StringProperty getSegundoNome() {
+		return segundoNome;
+	}
 
-		public ObjectProperty<LocalDate> getAniversario() {
-			return aniversario;
-		}
-	    
+	public StringProperty getRua() {
+		return rua;
+	}
+
+	public IntegerProperty getCep() {
+		return cep;
+	}
+
+	public StringProperty getCidade() {
+		return cidade;
+	}
+
+	public ObjectProperty<LocalDate> getAniversario() {
+		return aniversario;
+	}
+
 }
--- Contact/src/view/PersonOverviewController.java
+++ Contact/src/view/PersonOverviewController.java
@@ -10,57 +10,76 @@ import model.Pessoa;
 public class PersonOverviewController {
 
 	@FXML
-    private TableView<Pessoa> PessoaTable;
-    @FXML
-    private TableColumn<Pessoa, String> primeiroNomeColumn;
-    @FXML
-    private TableColumn<Pessoa, String> segundoNomeColumn;
-    
-    @FXML
-    private Label primerioNomeLabel;
-    @FXML
-    private Label segundoNomeLabel;
-    @FXML
-    private Label ruaLabel;
-    @FXML
-    private Label cepLabel;
-    @FXML
-    private Label cidadeLabel;
-    @FXML
-    private Label aniversarioLabel;
+	private TableView<Pessoa> PessoaTable;
+	@FXML
+	private TableColumn<Pessoa, String> primeiroNomeColumn;
+	@FXML
+	private TableColumn<Pessoa, String> segundoNomeColumn;
 
-    @SuppressWarnings("unused")
+	@FXML
+	private Label primerioNomeLabel;
+	@FXML
+	private Label segundoNomeLabel;
+	@FXML
+	private Label ruaLabel;
+	@FXML
+	private Label cepLabel;
+	@FXML
+	private Label cidadeLabel;
+	@FXML
+	private Label aniversarioLabel;
+
+	@SuppressWarnings("unused")
 	private MainApp mainApp;
-    
-    // Reference to the main application. 
-     //O construtor é chamado antes do método inicialize().
-     
-    public PersonOverviewController() {
-    }
 
-    /**
-     * Inicializa a classe controller. Este método é chamado automaticamente
-     *  após o arquivo fxml ter sido carregado.
-     */
-    @FXML
-    private void initialize() {
-        // Inicializa a tablea de pessoa com duas colunas. primerioNomeColumn
-        primeiroNomeColumn.setCellValueFactory(cellData -> cellData.getValue().getPrimeiroNome());
-        segundoNomeColumn.setCellValueFactory(cellData -> cellData.getValue().getSegundoNome());
-    }
+	// Reference to the main application.
+	// O construtor é chamado antes do método inicialize().
 
-    /**
-     * É chamado pela aplicação principal para dar uma referência de volta a si mesmo.
-     * 
-     * @param mainApp
-     */
-    
-    
-    public void setMainApp(MainApp mainApp) {
-        this.mainApp = mainApp;
+	public PersonOverviewController() {
+	}
 
-        // Adiciona os dados da observable list na tabela
-        PessoaTable.setItems(mainApp.getPessoaData());
-    }
+	/**
+	 * Inicializa a classe controller. Este método é chamado automaticamente após o
+	 * arquivo fxml ter sido carregado.
+	 */
+	@FXML
+	private void initialize() {
+		// Inicializa a tablea de pessoa com duas colunas. primerioNomeColumn
+		primeiroNomeColumn.setCellValueFactory(cellData -> cellData.getValue().getPrimeiroNome());
+		segundoNomeColumn.setCellValueFactory(cellData -> cellData.getValue().getSegundoNome());
+	}
+
+	/**
+	 * É chamado pela aplicação principal para dar uma referência de volta a si
+	 * mesmo.
+	 * 
+	 * @param mainApp
+	 */
+
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+
+		// Adiciona os dados da observable list na tabela
+		PessoaTable.setItems(mainApp.getPessoaData());
+	}
+
+	 private void showPersonDetails(Pessoa pessoa) {
+	 if (pessoa != null) {
+	 primerioNomeLabel.setText(pessoa.getPrimeiroNome());
+	// segundoNomeLabel.setText(pessoa.getSegundoNome());
+	// ruaLabel.setText(pessoa.getRua());
+	// cepLabel.setText(Integer.toString(pessoa.getCep()));
+	// cidadeLabel.setText(pessoa.getCidade());
+	//
+	 } else {
+	 primerioNomeLabel.setText("");
+	 segundoNomeLabel.setText("");
+	 ruaLabel.setText("");
+	 cepLabel.setText("");
+	 cidadeLabel.setText("");
+//	 aniversarioLabel.setText(DateUtil.format(pessoa.getAniversario()));
+	 }
 	
+	 }
+
 }
